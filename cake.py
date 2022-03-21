2fbd66cf08c9bc49d1d95882c67a98b72f292aa8
Juan720v <Juan.Vela@mail.citytech.cuny.edu>
2022-03-21 15:55:47 -0400

    version of cake on python

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeorbust = input("Do you want cake? (yes/no) ")
+
+if cakeorbust == "yes":
+    print("Have some cake")
+elif cakeorbust == "no":
+    print("No cake for you!")
+else:
+    print("Sorry, I don't understand your response....")
